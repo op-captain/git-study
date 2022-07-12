d2dbe280be89b13f2a0452ff114dd00f6c5d2c68
朱君 <zhujun@hongzhengtech.com>
2022-07-12 15:24:20 +0800

    1

--- test.js
+++ test.js
@@ -1,30 +1,3 @@
-/**
- *
- * @param array
- * @param start
- * @param end
- * @returns {any[]}
- */
-function baseSlice(array,start,end){
-    var index = -1,
-        length = array.length;
-
-    if(start < 0){
-        start = -start > length ? 0 : (length + start);
-    }
-
-    end  = end > length ? length : end;
-
-    var result = Array(length)
-
-    while (++index < length){
-        result[index] = array[index+start]
-    }
-
-    return result
-}
-
-var source = ['a',1,2,'b',{x:4}]
-
-console.log(baseSlice( source ))
-
+var a,
+    b,
+    c;
